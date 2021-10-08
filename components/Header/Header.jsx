847ada00e6f6ca410a2f1256efff61f92ea11917
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-08 16:24:58 -0500

    css-header-update

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -5,18 +5,45 @@ import Link from 'next/link';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 
+///styles components
+
+const Container = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    height: auto  !important;
+    margin: 0 auto;
+    @media (min-width:768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`
+
+const Logo = styled.a`
+    color: var(--naranja);
+    font-size: 3rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+    cursor: pointer;
+`;
+
+
+
 const Header = () => {
     return (
         <header
             css={css`
                 border: 2px solid var(--gris3);
-                padding: 1rem 0;
+                padding: 2.5rem 0;
             `
             }
         >
-            <div>
+            <Container >
                 <div>
-                    <p>P</p>
+                    <Link href='/'>
+                        <Logo>P</Logo>
+                    </Link>
                     <Search />
                     <Navagation />
                 </div>
@@ -26,7 +53,7 @@ const Header = () => {
                     <Link href='/'>Login</Link>
                     <Link href='/'>Cerrar secion</Link>
                 </div>
-            </div>
+            </Container >
         </header>
     )
 }
