b6604cb218065786b3da6357ebc66ae30a9f06fc
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-08 14:56:21 -0500

    css-to-header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import Search from '../ui/Search/Search';
 import Navagation from '../Navegation/Navagation';
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 
 const Header = () => {
     return (
-        <header>
+        <header
+            css={css`
+                border: 2px solid var(--gris3);
+                padding: 1rem 0;
+            `
+            }
+        >
             <div>
                 <div>
                     <p>P</p>
