e789b3e6a8b8393c9470cc769d6ac02e6c8bc7f8
Marcos <marcosbastidas301@gmail.com>
2021-10-26 01:10:58 -0400

    task_complete

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,114 +1,105 @@
 import React, { useState } from "react";
+import { useRouter } from 'next/router';
 import { css } from '@emotion/core';
 import Layout from '../components/Layout/Layout';
 import { Form, Field, InputSubmit,  Error } from '../components/ui/Form/Form';
 
-import firebaseApp from '../Firebase/Config';
-import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithRedirect,
-    GoogleAuthProvider,
-
-} from 'firebase/auth';
-
 import Firebase from '../Firebase/firebase';
 
-const auth = getAuth(firebaseApp);
+// validaciones
+import useValidation from '../hooks/useValidation';
+import validationCreateAcount from '../validation/ValidateCreateAcount'
 
-//const googleAuthProvider = new GoogleAuthProvider(); 
+
+const initialState = {
+  name: '',
+  email: '',
+  password: ''
+}
 
 const CreateAcount = () => {
-    const [ iseRegister, SetIsRegister ] = useState(true)
-    const [ email, setEmail ] = useState('');
-    const [password, setPassword] = useState('')
-    const [ user, setUser ]= useState(null)
-    console.log(user)
 
-    async function submitHandler(e) {
-        e.preventDefault();
-        
+    const [ error, guardarError] = useState(false);
+
+    const { values, errors, handleSubmit, handleChange, handleBlur } = useValidation(initialState, validationCreateAcount, crearCuenta);
+
+    const { name, email, password } = values;
+    const router = useRouter()
+
+    async function crearCuenta() {
         const res = await Firebase.register(email, password);
-        setUser(res)
-        
-        //const email= e.target.email.value;
-        //const password = e.target.password.value;
-        // if(iseRegister){
-        //     const res = await createUserWithEmailAndPassword(auth, email, password);
-        //     setUser(res)
-        // } else {
-        //     const res = await signInWithEmailAndPassword(auth, email, password);
-        //     setUser(res)
-        // }
-        
+        router.push('/');
     }
 
-  return (
-    
-    <>
-      <Layout>
+  
+    return (
       <>
-          <h1
-            css={css`
-              text-align: center;
-              margin-top: 5rem;
-            `}
-          >Crear Cuenta</h1>
-          <Form
-            onSubmit={submitHandler}
-            noValidate
-          >
-              <Field>
-                  <label htmlFor="nombre">Nombre</label>
-                  <input 
-                      type="text"
-                      id="name"
-                      placeholder="Tu Nombre"
-                      name="name"
-                      //value={name}
-                      //onChange={handleChange}
-                      //onBlur={handleBlur}
-                  />
-              </Field>
-
+        <Layout>
+        <>
+            <h1
+              css={css`
+                text-align: center;
+                margin-top: 5rem;
+              `}
+            >Crear Cuenta</h1>
+            <Form
+              onSubmit={handleSubmit}
+              noValidate
+            >
+                <Field>
+                    <label htmlFor="nombre">Nombre</label>
+                    <input 
+                        type="text"
+                        id="name"
+                        placeholder="Tu Nombre"
+                        name="name"
+                        value={name}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                </Field>
   
-              <Field>
-                  <label htmlFor="email">Email</label>
-                  <input 
-                      type="email"
-                      id="email"
-                      placeholder="Tu Email"
-                      name="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      //onBlur={handleBlur}
-                  />
-              </Field>
-  
-              <Field>
-                  <label htmlFor="password">Password</label>
-                  <input 
-                      type="password"
-                      id="password"
-                      placeholder="Tu Password"
-                      name="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      //onBlur={handleBlur}
-                  />
-              </Field>
-  
-              <InputSubmit 
-                type="submit"
-                value="Crear Cuenta"
-              />
-          </Form>
-        </>
-      </Layout>
-    </>
+                {errors.name && <Error>{errors.name}</Error> }
     
-  );
+                <Field>
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="email"
+                        id="email"
+                        placeholder="Tu Email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                </Field>
+                {errors.email && <Error>{errors.email}</Error> }
+    
+                <Field>
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password"
+                        id="password"
+                        placeholder="Tu Password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                </Field>
+                {errors.password && <Error>{errors.password}</Error> }
+  
+                {error && <Error>{error} </Error>}
+    
+                <InputSubmit 
+                  type="submit"
+                  value="Crear Cuenta"
+                />
+            </Form>
+          </>
+        </Layout>
+      </>
+    )
 };
 
 export default CreateAcount
